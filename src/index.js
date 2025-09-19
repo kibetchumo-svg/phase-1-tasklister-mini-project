cedcec2a06ee4353e0c58acaf10bbaf398d26139
kibetchumo-svg <kibet.chumo@student.moringaschool.com>
2025-09-19 10:55:45 +0300

    tasklister project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const input = document.getElementById("new-task-description").value;
+
+    if (input.trim() !== "") {
+      const li = document.createElement("li");
+      li.textContent = input; 
+      taskList.appendChild(li);
+
+      form.reset();
+    }
+  });
 });
